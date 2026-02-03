8af5522e576d99c0aa02c01a010c37c841d46a1c
Cedric <cedricctf11a@gmail.com>
2026-02-04 00:20:42 +0800

    setup rainbowkit providers

---
+++ lib/providers.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+import { chains } from './networkConfig';
+
+const config = getDefaultConfig({
+  appName: 'PolygonAkindo',
+  projectId: 'YOUR_PROJECT_ID', // Get your projectId from https://cloud.walletconnect.com/
+  chains,
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
