d5d441d728158246417e2df5564150dff3717ef3
Cedric <cedricctf11a@gmail.com>
2026-02-05 00:59:53 +0800

    added name wrapper address

--- lib/networkConfig.ts
+++ lib/networkConfig.ts
@@ -11,3 +11,6 @@ export const ETH_REGISTRAR_CONTROLLER_ADDRESS = '0xfb3cE5D01e0f33f41DbB39035dB97
 
 // ENS Public Resolver on Sepolia
 export const ENS_PUBLIC_RESOLVER_ADDRESS = '0xE99638b40E4Fff0129D56f03b55b6bbC4BBE49b5' as `0x${string}`;
+
+// ENS NameWrapper on Sepolia
+export const NAME_WRAPPER_ADDRESS = '0x0635513f179D50A207757E05759CbD106d7dFcE8' as `0x${string}`;
